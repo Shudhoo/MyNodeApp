79ab9666ac976b4f662f6ce944e4a931dee9e875
Shudhoo <140329641+Shudhoo@users.noreply.github.com>
2025-05-29 01:33:46 +0530

    react-env

--- client/src/login.jsx
+++ client/src/login.jsx
@@ -10,7 +10,7 @@ function Login() {
 
     const handleSubmit = (e) => {
         e.preventDefault()
-        axios.post('http://localhost:3001/login', { email, password})
+        axios.post('http://${process.env.REACT_APP_HOST_IP}:3001/login', { email, password})
         .then(result => {
             console.log(result)
             if(result.data === "Success") {
