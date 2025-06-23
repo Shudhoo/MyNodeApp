2709e195fc5e303808267d51e148a935e1a603d1
Shudhoo <140329641+Shudhoo@users.noreply.github.com>
2025-06-23 21:20:59 +0530

    project-done

--- client/src/home.jsx
+++ client/src/home.jsx
@@ -1,9 +1,18 @@
 import React from "react";
 
 function Home() {
-    return(
-        <h2> Developed and Deployed 3-Tier Application Successfully !!!!</h2>
-    )
+ return (
+    <div
+      className="bg-secondary d-flex justify-content-center align-items-center"
+      style={{ height: "100vh" }}
+    >
+      <div className="card shadow-sm p-4 text-center">
+        <h2 className="text-success">
+          Developed and Deployed 3-Tier Application Successfully !!!!
+        </h2>
+      </div>
+    </div>
+  );
 }
 
 export default Home;
